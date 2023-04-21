77d560ec0357dc08a0417d5170d65391a19402cb
Nick <nick.gregory.smith@gmail.com>
2023-04-21 12:58:02 -0500

    some updates following class

--- scripts/leaf_area_analysis.R
+++ scripts/leaf_area_analysis.R
@@ -12,11 +12,11 @@ imagej_path <- "/Applications/ImageJ.app/"
 leafimage_path <- "../data/leaf_area/scans/cropped"
 
 ## calculate leaf areas
-leaf_area <- run.ij(path.imagej = imagej_path, 
+leaf_area <- run.ij(path.imagej = imagej_path, # set imageJ path
                     set.directory = leafimage_path, # set paths
                     distance.pixel = 423, # set known pixel distance
                     known.distance = 15, # set known distance in cm
-                    save.image = F, # save the image to your folder
-                    check.image = F) # check image using software
+                    save.image = T, # save the image to your folder
+                    check.image = T) # check image using software
 
 # write.csv(leaf_area, file = "../data/leaf_area_raw.csv")
